6df560bf65c8c5e5853e2ffb126772a017f5ee13
telepath01 <47895030+telepath01@users.noreply.github.com>
2022-12-18 22:56:48 -0500

    Completed 1-7
    Completed exercises 1-7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degree) {
+  degree = (degree - 32) * 0.556;
+  const celsiusFixed = Number(degree.toFixed(0));
+  return celsiusFixed;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degree) {
+  degree = degree * 1.8 + 32;
+  const fahrenheitFixed = Number(degree.toFixed(0));
+  return fahrenheitFixed;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
